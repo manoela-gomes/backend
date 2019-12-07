2cb12f376d214ac3956882f17554103852f4cf5d
manoela-gomes <36117477+manoela-gomes@users.noreply.github.com>
2019-12-07 17:19:26 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require('cors');
 const routes = express.Router();
 const mongoose = require("mongoose");
 const LoginController = require("./controllers/LoginController");
@@ -8,6 +8,14 @@ const LoginController = require("./controllers/LoginController");
     return res.send('Hello World - testing again!');
 })*/
 // GET/PUT/DELETE/POST
+
+Tente adicionar em seu servidor node.js o módulo cors. E tente adicioná-lo desta forma:
+
+var express = require('express'),
+    cors = require('cors'),
+    app = express();
+
+app.use(cors());
 
 routes.post("/login", LoginController.store);
 
