a02e7ceda63cb115dddd561db3a4102782215d6d
manoela-gomes <manoela@aptas.com.br>
2019-12-12 09:20:04 -0300

    erro heroku

--- src/server.js
+++ src/server.js
@@ -28,4 +28,4 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(require('./routes').default);
 
-server.listen(process.env.PORT||3333);
+server.listen(process.env.PORT || 3333);
