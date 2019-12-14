6ee2ca596b7ec2c75a29d6db1679e2d11ae4137f
manoela-gomes <manoela@aptas.com.br>
2019-12-14 16:47:12 -0300

    colocando get

--- src/controllers/AcessoController.js
+++ src/controllers/AcessoController.js
@@ -1,14 +1,14 @@
 
 
 const mongoose = require("mongoose");
-const Acesso = require("../models/Acesso");
+const Login= require("../models/Login");
 
 
 class AcessoController{
 
     async store(req,res){
         var query = { login: this.req.login, password: this.req.password };
-        const acesso = await Acesso.find(query).toArray(function(err, result) {
+        const acesso = await Login.find(query).toArray(function(err, result) {
         return res.json(acesso);});
 
         //req.io.sockets.in(login)
