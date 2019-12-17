b68a636304a6d508e5ee2376cf03e613b9d32139
manoela-gomes <manoela@aptas.com.br>
2019-12-17 10:18:46 -0300

    resolvendo erro do acesso

--- src/controllers/AcessoController.js
+++ src/controllers/AcessoController.js
@@ -8,7 +8,7 @@ class AcessoController{
 
     async store(req,res){
         
-        const acesso = await Login.findOne({login:req.login},{_id: 1, login: 1, password: 1});
+        const acesso = await Login.findOne({login:req.login},{ login: 1, password: 1});
         return res.json(acesso);
 
         //req.io.sockets.in(login)
