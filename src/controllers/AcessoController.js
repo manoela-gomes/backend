189c2d87c4ceee1df63d2cdd9be30b6ae3b34056
manoela-gomes <manoela@aptas.com.br>
2019-12-19 16:59:53 -0300

    consertando erro login

--- src/controllers/AcessoController.js
+++ src/controllers/AcessoController.js
@@ -5,7 +5,7 @@ class AcessoController{
 
     async store(req,res){
 
-        const logar = await Login.find({login: req.body.login},{login:0, password:0});
+        const logar = await Login.find({login: req.body.login,password:req.body.password});
         return res.json(logar.password === req.body.password);
         //req.io.sockets.in(login)
     }
