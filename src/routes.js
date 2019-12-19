492657fdaed87c6c1797535827eafd63406be84b
manoela-gomes <manoela@aptas.com.br>
2019-12-19 15:21:20 -0300

    consertando erro login

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ var express = require('express'),
 const routes = express.Router();
 const mongoose = require("mongoose");
 const LoginController = require("./controllers/LoginController");
+const AcessoController = require("./controllers/AcessoController");
 
 /*routes.get('/teste',(req,res) => {
     return res.send('Hello World - testing again!');
@@ -14,6 +15,7 @@ const LoginController = require("./controllers/LoginController");
 var corsOptions = {
     origin: '*'
 };
+
 
 routes.post("/login", LoginController.store);
 routes.get("/logar", AcessoController.store);
